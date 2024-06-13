47183e25874ddf07ddd4ddee5cd365c659e8930b
Noah Syrkis <noah@syrkis.com>
2024-06-13 17:40:09 -0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<div class="container">
+    <h1>READOLENS</h1>
+    <span>
+        We're shutting down the service. Thanks for the support.
+    </span>
+    <br/>
+    <span>
+        Mihai & Noah
+    </span>
+</div>
+
+
+<style>
+    h1{
+        letter-spacing: 0.5rem;
+        font-size: 2.5rem;
+        font-weight: 100;
+    }
+    .container{
+        padding-top: 45vh;
+    }
+    :global(body) {
+        line-height:  2em;
+        text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        font-family: 'Crimson Pro', serif;
+        font-weight: 100;
+        font-size: 1.1em;
+        color: white;
+        background-image: url('https://syrkis.ams3.digitaloceanspaces.com/noah/aurelian/pearl.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        margin: 0;
+        padding: 0;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        text-align: center;
+    }
+</style>
