395e3c353dbb5cdc6388e8f6a1be5321bb701f6b
Noah Syrkis <noah@syrkis.com>
2025-04-30 10:06:49 +0200

    s3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,29 @@
 <div class="container">
     <h1>READOLENS</h1>
-    <span>
-        We're shutting down. Thanks for the support.
-    </span>
-    <br/>
-    <span>
-        Mihai & Noah
-    </span>
+    <span> We're shutting down — thanks for your support </span>
+    <br />
+    <span> Mihai & Noah </span>
 </div>
 
-
 <style>
-    h1{
+    h1 {
         letter-spacing: 0.5rem;
         font-size: 2.5rem;
         font-weight: 100;
     }
-    .container{
+    .container {
         padding-top: 45vh;
     }
-    :global(body) {
-        line-height:  2em;
-        font-family: 'Crimson Pro', serif;
-        font-weight: 100;
+    span {
         font-size: 1.1em;
+    }
+    :global(body) {
+        line-height: 2em;
+        font-family: "Libertinus Serif", serif;
+        font-weight: 100;
+        font-size: 1.2em;
         color: white;
-        background-image: url('https://syrkis.ams3.digitaloceanspaces.com/noah/aurelian/pearl.jpg');
+        background-image: url("https://s3.syrkis.com/aurelian/aurelian-4.jpg");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
